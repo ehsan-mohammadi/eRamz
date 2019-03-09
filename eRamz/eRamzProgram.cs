eb0ad04a3fb33b9ebe8de9a05ad08f125b0edc11
unknown <mohammadi.ehsan1994@gmail.com>
2019-03-09 14:45:40 +0330

    eRamz first commit

---
+++ eRamz/eRamzProgram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRamz
+{
+    class eRamzProgram
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if(args.Length == 3)
+                {
+                    string operation = args[0];
+                    string key = args[1];
+                    string path = args[2];
+
+                    // Start eRamz
+                    eRamzCore.ERamz(operation, key, path);
+                }
+                else
+                {
+                    eRamzCore.PrintERamz();
+                }
+            }
+            catch(Exception)
+            {
+                // Print eRamz
+                eRamzCore.PrintERamz();
+            }
+        }
+    }
+}
